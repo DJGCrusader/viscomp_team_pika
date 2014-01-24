216cc6dbf0b4f3d8eb4e9e79346ab60c912769be
dalitso banda <dbanda@mit.edu>
2014-01-24 18:09:07 -0500

    added get confidence method

---
+++ devkit/getConfidence.m
@@ -0,0 +1,4 @@
+%%
+% getConfidence is  method that takes an array of images names and a list of the classes. It returns a confidence matrix of how 
+% confident an image is classified in each class.  The the confidence matrix is of size length(image names) X length(classes)
+%%
